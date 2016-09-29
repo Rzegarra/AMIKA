64c882cca507cdec37e5d9fbfffe23be9f2921f3
Rafael Zegarra <rafael.zegarra@ucsp.edu.pe>
2016-09-28 22:44:49 -0500

    haciendo pruebas con nginx en windows

--- TEST_nginx/app.js
+++ TEST_nginx/app.js
@@ -1,16 +1,23 @@
 var express    = require('express')
 var bodyParser = require('body-parser')
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 var app = express()
 
 // parse application/json
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', function(req, res){
+  res.send('AMICA :) ' );
+  console.log('hola')
+});
+
 
 app.use(function (req, res, next) {
   console.log(req.body) // populated!
-  res.send('llego ok '+req.body[0])
   next()
 })
+
+
 app.listen(port);
 console.log('Server started! At http://localhost:' + port);
